4d422ce6a443164ed21eb9b94d45336bb683b6c0
unknown <hannanmalik1999@gmail.com>
2023-01-21 16:27:32 +0100

    Basic JWT token

--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,18 @@
+const CustomAPIError = require("../errors/custom-error")
+const jwt = require("jsonwebtoken")
 
 const login = async (req,res)=>{
-    res.send("Fake login/Register/Signup Route")
+    const {username, password} = req.body
+    if(!username || !password){
+    throw new CustomAPIError("Please provide email and password", 400)
     }
+    const id = new Date().getDate()
+    const token = jwt.sign({id,username}, process.env.JWT_SECRET,{expiresIn:"30d"})
+    console.log(username, password)
+    res.status(200).json({msg: "User Created!", token})
+    }
+
+
     
     const dashboard = async (req,res)=>{
         const luckyNumber = Math.floor(Math.random()* 100)
